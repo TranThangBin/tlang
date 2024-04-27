4bd6086ce3fdee909bf3b352c43f6fa610c9bb12
TranThangBin <thangdev04@gmail.com>
2024-04-27 16:39:29 +0700

    feat: binary operation for string literal

--- src/runtime/evaluate-binary-operation.cpp
+++ src/runtime/evaluate-binary-operation.cpp
@@ -4,6 +4,11 @@
 #include <memory>
 #include <stdexcept>
 
+std::runtime_error GetError(BinaryOperator op) {
+  throw std::runtime_error("Unexpected binary operator " +
+                           BinaryOperatorToString(op));
+}
+
 std::shared_ptr<RuntimeValue>
 evaluateNumericOperation(std::shared_ptr<NumberValue> leftHand,
                          std::shared_ptr<NumberValue> rightHand,
@@ -33,8 +38,21 @@ evaluateNumericOperation(std::shared_ptr<NumberValue> leftHand,
                                          (int)rightHand->GetValue());
 
   default:
-    throw std::runtime_error("Unexpected binary operator " +
-                             BinaryOperatorToString(op));
+    throw GetError(op);
+  }
+}
+
+std::shared_ptr<RuntimeValue>
+evaluateStringOperation(std::shared_ptr<StringValue> leftHand,
+                        std::shared_ptr<StringValue> rightHand,
+                        BinaryOperator op) {
+  switch (op) {
+  case BinaryOperator::Addition:
+    return std::make_shared<StringValue>(leftHand->GetValue() +
+                                         rightHand->GetValue());
+
+  default:
+    throw GetError(op);
   }
 }
 
@@ -49,6 +67,11 @@ Interpreter::evaluateBinaryOperation(std::shared_ptr<RuntimeValue> left,
     return evaluateNumericOperation(
         std::static_pointer_cast<NumberValue>(left),
         std::static_pointer_cast<NumberValue>(right), op);
+
+  case DataType::String:
+    return evaluateStringOperation(std::static_pointer_cast<StringValue>(left),
+                                   std::static_pointer_cast<StringValue>(right),
+                                   op);
 
   default:
     throw std::runtime_error("No binary operator " +
